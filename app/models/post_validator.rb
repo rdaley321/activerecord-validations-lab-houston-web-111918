068ef94bd35363815d6e9ad6c5959607107b6b57
Ryan Daley <rdaley321@yahoo.com>
2018-12-20 19:15:59 +0000

    Automatically backed up by Learn

--- app/models/post_validator.rb
+++ app/models/post_validator.rb
@@ -1,6 +1,6 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
-    if !(post.title.include? "Won't Believe" || post.title.include? "Secret" || post.title.include? "Top [number]" || post.title.include? "Guess")
+    if !(post.title.include?("Won't Believe") || post.title.include?("Secret") || post.title.include?("Top [number]") || post.title.include?("Guess")
         record.errors[:name] << 'Make your title more clickbait!'
     end
   end
