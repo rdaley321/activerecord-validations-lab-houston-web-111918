e449b1c73a806d8da1f3403661ebf27580ce811f
Ryan Daley <rdaley321@yahoo.com>
2018-12-20 19:00:24 +0000

    Automatically backed up by Learn

--- app/models/post_validator.rb
+++ app/models/post_validator.rb
@@ -1,6 +1,6 @@
 class MyValidator < ActiveModel::Validator
   def validate(record)
-    unless post.title.includes? "Won't Believe"
+    unless post.title.includes? "Won't Believe" || "Secret" ||
     unless post.title.includes? "Secret"
     unless post.title.includes? "Top [number]"
     unless post.title.includes? "Guess"
