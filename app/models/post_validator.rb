42ef49e2596348ccc68bdc6e4af7cdaf23036047
Ryan Daley <rdaley321@yahoo.com>
2018-12-20 19:11:49 +0000

    Automatically backed up by Learn

--- app/models/post_validator.rb
+++ app/models/post_validator.rb
@@ -1,5 +1,7 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
     unless post.title.include? "Won't Believe" || post.title.include? "Secret" || post.title.include? "Top [number]" || post.title.include? "Guess"
+    
+    end
   end
 end
